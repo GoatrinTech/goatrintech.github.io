edcac3863eecdb1642d5ef751890fadaa131b601
GoatrinTech <goatrintech25@proton.me>
2025-05-16 16:44:42 -0400

    update files

--- wipScript.js
+++ wipScript.js
@@ -1,10 +1,10 @@
-const images = ['/images/LD.jpeg', '/images/b1.jpg', 'b2.jpg'];
-const imageElement = document.getElementById('ldimage');
+var imagesArray = ["/images/LD.jpeg", "b1.jpg", "b2.jpg"];
 
-function getRandomImage() {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    randomImage = images[randomIndex];
-    imageElement.src = randomImage;
+function displayImage(){
+
+    //the first statement should generate a random number in the range 0 to 6 (the subscript values of the image file names in the imagesArray)
+    var num = Math.floor(Math.random() * (imagesArray.length+1));
+    //the second statement display the random image from the imagesArray array in the canvas image using the random number as the subscript value
+    document.canvas.src = imagesArray[num];
+
 }
-
-getRandomImage();
