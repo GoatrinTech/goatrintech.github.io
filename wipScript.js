2f48a6ae065e0fa680c68f726357a10b48d43968
GoatrinTech <goatrintech25@proton.me>
2025-05-16 15:15:23 -0400

    upload and update files

---
+++ wipScript.js
@@ -0,0 +1,10 @@
+const images = ['/images/LD.jpeg', '/images/b1.jpg', 'b2.jpg'];
+const imageElement = document.getElementById('ldimage');
+
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    imageElement.src = randomImage;
+}
+
+getRandomImage();
